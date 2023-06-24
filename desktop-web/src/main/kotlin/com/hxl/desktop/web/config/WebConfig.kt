6f73cab5ffc83bb5de9838936c7951ae9e23b824
2606710413@qq.com <2606710413@qq.com>
2023-06-24 10:49:19 +0800

    fix

--- desktop-web/src/main/kotlin/com/hxl/desktop/web/config/WebConfig.kt
+++ desktop-web/src/main/kotlin/com/hxl/desktop/web/config/WebConfig.kt
@@ -1,13 +1,9 @@
 package com.hxl.desktop.web.config
 
-import com.hxl.desktop.common.core.Directory
-import com.hxl.desktop.common.kotlin.extent.toFile
-import com.hxl.desktop.file.extent.writeStringBuffer
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.web.server.ErrorPage
 import org.springframework.boot.web.server.WebServerFactoryCustomizer
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
